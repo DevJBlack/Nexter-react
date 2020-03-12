4c1e89690242f10da4f8a7e4d71db16cd11e8a95
Devin Black <Devinjamesblack@gmail.com>
2020-03-12 05:58:40 -0600

    starting grids

--- src/Components/Application/Features.js
+++ src/Components/Application/Features.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { ReactComponent as Sprit1} from '../Img/SVG/heart-full.svg'
 
 const Features = () => {
   return (
-    <features className="features">
-      Features
-    </features>
+    <div className="features">
+      <div className="feature">
+        <svg className="feature__icon">
+          <Sprit1 />
+        </svg>
+      </div>
+      <div className="feature">feature 2</div>
+      <div className="feature">feature 3</div>
+      <div className="feature">feature 4</div>
+      <div className="feature">feature 5</div>
+      <div className="feature">feature 6</div>
+    </div>
   )
 }
 

4550cbb20e24669dc2c6f4334045dea12d4878e0
Devin Black <Devinjamesblack@gmail.com>
2020-03-13 06:01:27 -0600

    svg added

--- src/Components/Application/Features.js
+++ src/Components/Application/Features.js
@@ -1,19 +1,48 @@
 import React from 'react'
-import { ReactComponent as Sprit1} from '../Img/SVG/heart-full.svg'
+import { ReactComponent as Global1} from '../Img/SVG/icon-global.svg'
+import { ReactComponent as Trophy1} from '../Img/SVG/icon-trophy.svg'
+import { ReactComponent as Key1} from '../Img/SVG/icon-key.svg'
+import { ReactComponent as Presentation1} from '../Img/SVG/icon-presentation.svg'
+import { ReactComponent as Lock1} from '../Img/SVG/icon-lock.svg'
 
 const Features = () => {
   return (
     <div className="features">
       <div className="feature">
         <svg className="feature__icon">
-          <Sprit1 />
+          <Global1 />
         </svg>
+        <h4 className="heading-4">World's best luxury homes</h4>
+        <p className="feature__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.</p>
       </div>
-      <div className="feature">feature 2</div>
-      <div className="feature">feature 3</div>
-      <div className="feature">feature 4</div>
-      <div className="feature">feature 5</div>
-      <div className="feature">feature 6</div>
+      <div className="feature">
+        <svg className="feature__icon">
+          <Trophy1 />
+        </svg>
+        <h4 className="heading-4">Only the best property</h4>
+        <p className="feature__text">Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.</p>
+      </div> 
+      <div className="feature">
+        <svg className="feature__icon">
+          <Key1 />
+        </svg>
+        <h4 className="heading-4">New home in one week</h4>
+        <p className="feature__text">Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.</p>
+      </div> 
+      <div className="feature">
+        <svg className="feature__icon">
+          <Presentation1 />
+        </svg>
+        <h4 className="heading-4">Top 1% realtors</h4>
+        <p className="feature__text">Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div> 
+      <div className="feature">
+        <svg className="feature__icon">
+          <Lock1 />
+        </svg>
+        <h4 className="heading-4">World's best homes</h4>
+        <p className="feature__text">Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.</p>
+      </div>
     </div>
   )
 }

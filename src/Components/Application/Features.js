5e3d40335fcdb6bf6cbba215f7f8db7eb5614886
Devin Black <Devinjamesblack@gmail.com>
2020-03-15 22:29:23 -0600

    added svg

--- src/Components/Application/Features.js
+++ src/Components/Application/Features.js
@@ -4,6 +4,7 @@ import { ReactComponent as Trophy1} from '../Img/SVG/icon-trophy.svg'
 import { ReactComponent as Key1} from '../Img/SVG/icon-key.svg'
 import { ReactComponent as Presentation1} from '../Img/SVG/icon-presentation.svg'
 import { ReactComponent as Lock1} from '../Img/SVG/icon-lock.svg'
+import { ReactComponent as Map1} from '../Img/SVG/icon-map-pin.svg'
 
 const Features = () => {
   return (
@@ -15,6 +16,7 @@ const Features = () => {
         <h4 className="heading-4">World's best luxury homes</h4>
         <p className="feature__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.</p>
       </div>
+
       <div className="feature">
         <svg className="feature__icon">
           <Trophy1 />
@@ -22,6 +24,15 @@ const Features = () => {
         <h4 className="heading-4">Only the best property</h4>
         <p className="feature__text">Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.</p>
       </div> 
+
+      <div className="feature">
+        <svg className="feature__icon">
+          <Map1 />
+        </svg>
+        <h4 className="heading-4">Only the best property</h4>
+        <p className="feature__text">Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.</p>
+      </div>
+
       <div className="feature">
         <svg className="feature__icon">
           <Key1 />
@@ -29,6 +40,7 @@ const Features = () => {
         <h4 className="heading-4">New home in one week</h4>
         <p className="feature__text">Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.</p>
       </div> 
+
       <div className="feature">
         <svg className="feature__icon">
           <Presentation1 />
@@ -36,11 +48,12 @@ const Features = () => {
         <h4 className="heading-4">Top 1% realtors</h4>
         <p className="feature__text">Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div> 
+
       <div className="feature">
         <svg className="feature__icon">
           <Lock1 />
         </svg>
-        <h4 className="heading-4">World's best homes</h4>
+        <h4 className="heading-4">Secure payments on nexter</h4>
         <p className="feature__text">Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.</p>
       </div>
     </div>

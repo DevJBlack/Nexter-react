f1832d10a202554cd3b3cc44d7ff0bf681d64524
Devin Black <Devinjamesblack@gmail.com>
2020-03-18 06:40:40 -0600

    content pictures

--- src/Components/Application/StoryPictures.js
+++ src/Components/Application/StoryPictures.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import story1 from '../Img/story-1.jpeg'
+import story2 from '../Img/story-2.jpeg'
 
 const StoryPictures = () => {
   return (
     <div className="story__pictures">
-      Story__Pictures
+      <img src={story1} alt="Couple with new House" className="story__img--1"/>
+      <img src={story2} alt="New House" className="story__img--2"/>
     </div>
   )
 }

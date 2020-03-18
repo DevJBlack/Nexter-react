fcc2092a08a52f75cbb548c0a9b9659bf0607ffa
Devin Black <Devinjamesblack@gmail.com>
2020-03-18 07:01:08 -0600

    home section

--- src/Components/Application/Homes.js
+++ src/Components/Application/Homes.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import house1 from '../Img/house-1.jpeg'
+import { ReactComponent as Heart } from '../Img/SVG/heart-full.svg'
+import { ReactComponent as Map } from '../Img/SVG/icon-map.svg'
 
 const Homes = () => {
   return (
     <div className="homes">
-      Homes
+      <div className="home">
+        <img src={house1} alt="House One" className="home__img"/>
+        <svg className="home__like">
+          <Heart />
+        </svg>
+        <h5 className="home__name">Beautiful Family House</h5>
+        <div className="home__location">
+        <svg className="home__like">
+          <Map />
+        </svg>
+        </div>
+      </div>
+
+      <div className="home">Home card 2</div>
+
+      <div className="home">Home card 3</div>
+
+      <div className="home">Home card 4</div>
+
+      <div className="home">Home card 5</div>
+
+      <div className="home">Home card 6</div>
     </div>
   )
 }

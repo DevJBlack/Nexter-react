af5138aba19f25cb43687092e74526e58aca110f
Devin Black <Devinjamesblack@gmail.com>
2020-03-20 09:16:30 -0600

    started gallary

--- src/Components/Application/Gallery.js
+++ src/Components/Application/Gallery.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import gal1 from '../Img/gal-1.jpeg'
+import gal2 from '../Img/gal-2.jpeg'
+import gal3 from '../Img/gal-3.jpeg'
+import gal4 from '../Img/gal-4.jpeg'
+import gal5 from '../Img/gal-5.jpeg'
+import gal6 from '../Img/gal-6.jpeg'
+import gal7 from '../Img/gal-7.jpeg'
+import gal8 from '../Img/gal-8.jpeg'
+import gal9 from '../Img/gal-9.jpeg'
+import gal10 from '../Img/gal-10.jpeg'
+import gal11 from '../Img/gal-11.jpeg'
+import gal12 from '../Img/gal-12.jpeg'
+import gal13 from '../Img/gal-13.jpeg'
+import gal14 from '../Img/gal-14.jpeg'
 
 const Gallery = () => {
   return (
     <section className="gallery">
-      Gallery
+      <figure className="gallery__item gallery__item--1">
+        <img src={gal1} alt="Gal 1" className="gallery__img"/>
+      </figure>
+
+
     </section>
   )
 }

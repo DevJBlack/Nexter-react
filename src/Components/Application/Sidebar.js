d75e7cbcfb227a4253e68fa860d62f7f30c076ba
Devin Black <Devinjamesblack@gmail.com>
2020-03-21 06:56:52 -0600

    sidebar

--- src/Components/Application/Sidebar.js
+++ src/Components/Application/Sidebar.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Sidebar = () => {
   return (
       <div className="sidebar">
-        SideBar
+        <button className="nav-btn"></button>
+
+        {/* <div className="sidebar__container">
+          <div className="sidebar__line--1"></div>
+          <div className="sidebar__line--2"></div>
+          <div className="sidebar__line--3"></div>
+        </div> */}
       </div>
   )
 }

663561e8655a363425f0e36ba1dc5b38279c80ce
Devin Black <Devinjamesblack@gmail.com>
2020-03-23 07:12:34 -0600

    realtors

--- src/Components/Application/Header.js
+++ src/Components/Application/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header className="header">
       <img src={logo} alt="Nexter Logo" className="header__logo"/>
-      <h3 className="header-3">Your own home</h3>
+      <h3 className="heading-3">Your own home</h3>
       <h1 className="heading-1">The ultimate personal freedom</h1>
       <button className="btn header__btn">View our properties</button>
       <div className="header__seenon-text">Seen on</div>
--- src/Components/Application/Realtors.js
+++ src/Components/Application/Realtors.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import realtor1 from '../Img/realtor-1.jpeg'
+import realtor2 from '../Img/realtor-2.jpeg'
+import realtor3 from '../Img/realtor-3.jpeg'
 
 const Realtors = () => {
   return (
     <div className="realtors">
-      Realtors
+      <h3 className="heading-3">Top 3 Realtors</h3>
+      <div className="realtors__list">
+        <img src={realtor1} alt="Realtor One" className="realtors__img"/>
+        <div className="realtor__details">
+          <h4 className="heading-4 heading-4--light">Erik Felman</h4>
+          <p className="realtors__sold">245 Houses</p>
+        </div>
+
+        <img src={realtor2} alt="Realtor Two" className="realtors__img"/>
+        <div className="realtor__details">
+          <h4 className="heading-4 heading-4--light">Kim Brown</h4>
+          <p className="realtors__sold">212 Houses</p>
+        </div>
+
+        <img src={realtor3} alt="Realtor Threes" className="realtors__img"/>
+        <div className="realtor__details">
+          <h4 className="heading-4 heading-4--light">Toby Ramsey</h4>
+          <p className="realtors__sold">198 Houses</p>
+        </div>
+      </div>
     </div>
   )
 }
